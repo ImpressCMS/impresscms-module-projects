7823f534fbaa6c137407a7f1330f47c9560a38c4
Simon <simon@isengard.biz>
2012-02-06 07:01:31 +0000

    Remove redundant global.
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/projects/trunk@23378 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- include/onupdate.inc.php
+++ include/onupdate.inc.php
@@ -38,8 +38,6 @@ function projects_db_upgrade_2() {
 
 function icms_module_update_projects($module)
 {
-	global $xoopsDB;
-
 	// create an uploads directory for logos
 	$path = ICMS_ROOT_PATH . '/uploads/' . basename(dirname(dirname(__FILE__)));
 	$directory_exists = $file_exists = $writeable = TRUE;

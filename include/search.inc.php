1b2ccbab22ae72bad74e05734d90f497f379c16d
Simon <simon@isengard.biz>
2013-02-11 07:32:24 +0000

    Improve efficiency of search function.
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/projects/trunk@24887 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- include/search.inc.php
+++ include/search.inc.php
@@ -14,22 +14,47 @@
 
 defined("ICMS_ROOT_PATH") or die("ICMS root path not defined");
 
-function projects_search($queryarray, $andor, $limit, $offset, $userid)
+function projects_search($queryarray, $andor, $limit, $offset = 0, $userid = 0)
 {
+	global $icmsConfigSearch;
+	
+	$projectsArray = $ret = array();
+	$count = $number_to_process = $projects_left = '';
+	
 	$projects_project_handler = icms_getModuleHandler("project", basename(dirname(dirname(__FILE__))), "projects");
-	$projectArray = $projects_project_handler->getProjectsForSearch($queryarray, $andor, $limit, $offset, $userid);
-	$ret = array();
-
-	foreach ($projectArray as $project) 
+	$projectsArray = $projects_project_handler->getProjectsForSearch($queryarray, $andor, $limit, $offset, $userid);
+	
+	// Count the number of records
+	$count = count($projectsArray);
+	
+	// The number of records actually containing project objects is <= $limit, the rest are padding
+	$projects_left = ($count - ($offset + $icmsConfigSearch['search_per_page']));
+	if ($projects_left < 0) {
+		$number_to_process = $icmsConfigSearch['search_per_page'] + $projects_left; // $projects_left is negative
+	} else {
+		$number_to_process = $icmsConfigSearch['search_per_page'];
+	}
+	
+	// Process the actual projects (not the padding)
+	for ($i = 0; $i < $number_to_process; $i++)
 	{
 		$item['image'] = "images/project.png";
-		$item['link'] = $project->getItemLink(TRUE);
-		$item['title'] = $project->getVar("title");
-		$item['time'] = $project->getVar("date", "e");
-		$item['uid'] = $project->getVar("creator");
+		$item['link'] = $projectsArray[$i]->getItemLink(TRUE);
+		$item['title'] = $projectsArray[$i]->getVar("title");
+		$item['time'] = $projectsArray[$i]->getVar("date", "e");
+		$item['uid'] = $projectsArray[$i]->getVar("creator");
 		$ret[] = $item;
 		unset($item);
 	}
 
+	// Restore the padding (required for 'hits' information and pagination controls). The offset
+	// must be padded to the left of the results, and the remainder to the right or else the search
+	// pagination controls will display the wrong results (which will all be empty).
+	// Left padding = -($limit + $offset)
+	$ret = array_pad($ret, -($offset + $number_to_process), 1);
+	
+	// Right padding = $count
+	$ret = array_pad($ret, $count, 1);
+	
 	return $ret;
 }
